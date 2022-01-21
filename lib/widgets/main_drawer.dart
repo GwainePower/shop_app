c9e70ed24660361994187514885f876351de019c
ArtemiyBTR <artemiybtr@oxygen>
2022-01-21 21:00:33 +0300

    fix

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/helpers/custom_route.dart';
 
 import '../providers/auth_provider.dart';
 
